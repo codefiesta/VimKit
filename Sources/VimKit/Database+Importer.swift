8fe4e9e80209a4fb0ff3eaf39491ca25195f3ed7
Kevin McKee <kevinmmckee@gmail.com>
2024-08-22 05:45:34 -0700

    Added function to compute the vertex normals on the GPU.

--- Sources/VimKit/Database+Importer.swift
+++ Sources/VimKit/Database+Importer.swift
@@ -336,15 +336,3 @@ extension Database {
         }
     }
 }
-
-extension TimeInterval {
-
-    func stringFromTimeInterval() -> String {
-        let time = Int(self)
-        let ms = Int((self.truncatingRemainder(dividingBy: 1)) * 1000)
-        let seconds = time % 60
-        let minutes = (time / 60) % 60
-        let hours = (time / 3600)
-        return String(format: "%0.2d:%0.2d:%0.2d.%0.3d", hours, minutes, seconds, ms)
-    }
-}
