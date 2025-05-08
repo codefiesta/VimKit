d6ae2fc649fa09e6855a823391bc7e3b2aa16804
Kevin McKee <kevinmmckee@gmail.com>
2025-05-07 20:11:35 -0700

    String subscript extensions

--- Sources/VimKit/Extensions/String+Extensions.swift
+++ Sources/VimKit/Extensions/String+Extensions.swift
@@ -22,5 +22,25 @@ public extension String {
     /// Convenience var that trim all whitespace.
     var trimmed: String {
         trimmingCharacters(in: .whitespaces)
-   }
+    }
+
+    /// Convenience subscript using a countable closed range.
+    /// - Parameters:
+    ///   - bounds: the bounds to subscript
+    /// - Returns: a string at the specified bounds
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+
+    /// Convenience subscript using a countable range.
+    /// - Parameters:
+    ///   - bounds: the bounds to subscript
+    /// - Returns: a string at the specified bounds
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
 }
