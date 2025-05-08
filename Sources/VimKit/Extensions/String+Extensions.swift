0416f3b64a87fed73907df8d83a388db3dcef21b
Code Fiesta <17772633+codefiesta@users.noreply.github.com>
2025-05-07 20:16:35 -0700

    Database Areas + String Utils (#104)

--- Sources/VimKit/Extensions/String+Extensions.swift
+++ Sources/VimKit/Extensions/String+Extensions.swift
@@ -18,4 +18,29 @@ public extension String {
         let hashed = SHA256.hash(data: Data(self.utf8))
         return hashed.compactMap { String(format: "%02x", $0) }.joined()
     }
+
+    /// Convenience var that trim all whitespace.
+    var trimmed: String {
+        trimmingCharacters(in: .whitespaces)
+    }
+
+    /// Convenience subscript using a countable closed range.
+    /// - Parameters:
+    ///   - bounds: the bounds to subscript
+    /// - Returns: a string at the specified bounds
+    subscript (bounds: CountableClosedRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start...end])
+    }
+
+    /// Convenience subscript using a countable range.
+    /// - Parameters:
+    ///   - bounds: the bounds to subscript
+    /// - Returns: a string at the specified bounds
+    subscript (bounds: CountableRange<Int>) -> String {
+        let start = index(startIndex, offsetBy: bounds.lowerBound)
+        let end = index(startIndex, offsetBy: bounds.upperBound)
+        return String(self[start..<end])
+    }
 }
